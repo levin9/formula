562f34da0e84030c4ea26a850ff3ef8e7a8a352a
levin9 <sz.lizhigang@foxmail.com>
2020-01-01 23:44:13 +0800

    添加 equal函数

---
+++ internal/fs/equalFunction.go
@@ -0,0 +1,49 @@
+package fs
+
+import (
+	"fmt"
+	"github.com/levin9/formula/opt"
+	"reflect"
+)
+
+type equalFunction struct {
+}
+
+func (*equalFunction) Name() string {
+	return "=="
+}
+
+func (f *equalFunction) Evaluate(context *opt.FormulaContext, args ...*opt.LogicalExpression) (*opt.Argument, error) {
+
+	err := opt.MatchTwoArgument(f.Name(), args...)
+	if err != nil {
+		return nil, err
+	}
+
+	arg0, err := (*args[0]).Evaluate(context)
+	if err != nil {
+		return nil, err
+	}
+
+	if !arg0.IsNumber() {
+		return nil, fmt.Errorf("function %s required numbers", f.Name())
+	}
+
+	arg1, err := (*args[1]).Evaluate(context)
+	if err != nil {
+		return nil, fmt.Errorf("function %s required numbers", f.Name())
+	}
+
+	v0, _ := arg0.Float64()
+	v1, _ := arg1.Float64()
+
+	if v0 == v1 {
+		return opt.NewArgumentWithType(true, reflect.Bool), nil
+	}
+
+	return opt.NewArgumentWithType(false, reflect.Bool), nil
+}
+
+func NewEqualFunction() *equalFunction {
+	return &equalFunction{}
+}
